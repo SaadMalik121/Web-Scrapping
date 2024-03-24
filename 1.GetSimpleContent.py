409e0a4009dfccfa161392c55fb07e5614959765
SaadMalik <qadeer@cheetahagency.com>
2024-03-24 23:48:51 +0500

    Scrap tabular data

--- 1.GetSimpleContent.py
+++ 1.GetSimpleContent.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 
 r = requests.get("https://webscraper.io/test-sites/e-commerce/allinone")
 
+# printing r is important to check whether the website allow us to scrap data or not
+# print(r)
+
 soup=BeautifulSoup(r.text,"lxml")
 
 p = soup.find("div",class_="jumbotron").h1.string
